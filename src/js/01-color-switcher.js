29baea88de646ae1245e4d3066ec84e19e284351
Shveps96 <42212039+Shveps96@users.noreply.github.com>
2022-12-12 01:46:14 +0200

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const { startBtn, stopBtn, buttons } = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
 
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  startBtn.setAttribute('disabled', true);
+  timerId = setInterval(() => {
+    const bodyBgColor = getRandomHexColor();
+    document.body.style.backgroundColor = bodyBgColor;
+  }, 1000);
+}
+
+function onClickStop() {
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
